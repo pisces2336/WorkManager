a1017ef80b4f65f3acfffce5f516dfad36326f54
pisces2336 <pisces2336@gmail.com>
2023-02-22 07:53:52 +0000

    fix: add development application url to config.hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "fe5997b83f174283a1c168f11c344fe4.vfs.cloud9.us-east-1.amazonaws.com"
 end
