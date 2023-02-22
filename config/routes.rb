961f955cb7477c4ac0acd0d742a167434b689db3
pisces2336 <pisces2336@gmail.com>
2023-02-22 11:59:25 +0000

    chore: generate WorksController
    index, show, new, edit, create, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'works/index'
+  get 'works/show'
+  get 'works/new'
+  get 'works/edit'
+  get 'works/create'
+  get 'works/update'
+  get 'works/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'users#show'
