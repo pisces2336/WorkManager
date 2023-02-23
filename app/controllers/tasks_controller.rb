8e990ed7f8990c37e1ccf7667b730dbdabccf1ee
pisces2336 <pisces2336@gmail.com>
2023-02-23 07:21:53 +0000

    fix: edit the routes
    nest as 'works > tasks', and fix some relevant part

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,24 @@
 class TasksController < ApplicationController
   def new
     @task = Task.new
-    work = Work.find_by_id(params[:work_id])
+    @work = Work.find(params[:work_id])
 
-    if work.nil? || work.user != current_user
+    if @work.user != current_user
       redirect_to user_path
     end
-
-    @task.work = work
   end
 
   def edit
   end
 
   def create
-    
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to work_path(@task.work)
+    else
+      @work = @task.work
+      render :new
+    end
   end
 
   def update
@@ -25,6 +29,6 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :scheduled_date, :due_date)
+    params.require(:task).permit(:title, :scheduled_date, :due_date).merge(work_id: params[:work_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
-  get 'tasks/new'
-  get 'tasks/edit'
-  get 'tasks/create'
-  get 'tasks/update'
-  get 'tasks/destroy'
-  get 'works/index'
-  get 'works/show'
-  get 'works/new'
-  get 'works/edit'
-  get 'works/create'
-  get 'works/update'
-  get 'works/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'users#show'
   resource :user, only: [:show]
-  resources :works
-  resources :tasks
+  resources :works do
+    resources :tasks
+  end
 end
