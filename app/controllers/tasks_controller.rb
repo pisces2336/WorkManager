6dc4dadae0731f9ef1799a6cbd0b0ee774a2c452
pisces2336 <pisces2336@gmail.com>
2023-02-23 07:03:45 +0000

    feat[WIP]: add tasks#new, create
    I need fix the routes for tesks' form (tasks are haven by works)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,30 @@
 class TasksController < ApplicationController
   def new
+    @task = Task.new
+    work = Work.find_by_id(params[:work_id])
+
+    if work.nil? || work.user != current_user
+      redirect_to user_path
+    end
+
+    @task.work = work
   end
 
   def edit
   end
 
   def create
+    
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :scheduled_date, :due_date)
   end
 end
