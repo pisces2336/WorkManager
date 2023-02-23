4765ee929432e2a49b12ae2ecebf7551065019f4
pisces2336 <pisces2336@gmail.com>
2023-02-23 04:02:26 +0000

    feat: add works#create and work_params

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -13,11 +13,22 @@ class WorksController < ApplicationController
   end
 
   def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work)
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def work_params
+    params.require(:work).permit(:client_name, :contact_url, :document_url, :due, :memo).merge(user_id: current_user.id)
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,6 @@
 class Work < ApplicationRecord
   belongs_to :user
   has_many :tasks
+
+  validates :client_name, presence: true
 end
