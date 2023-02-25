f60b320ab672a185ae3b11b5b3744d88e774e2fa
pisces2336 <pisces2336@gmail.com>
2023-02-25 08:54:07 +0000

    feat: add navbar (header)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'users#show'
-  resource :user, only: [:show]
+  root to: 'users#show'
   resources :works do
     resources :tasks
   end
