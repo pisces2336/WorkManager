bd87881704cc0ececa1ca8b18cc48d9982dfd3ce
pisces2336 <pisces2336@gmail.com>
2023-02-25 06:23:51 +0000

    feat: add users#show
    add task calendar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @tasks = Task.all.order(:scheduled_date).order(:title)
+
+    @first_date = @tasks.first.scheduled_date
+    @last_date = @tasks.last.scheduled_date
   end
 end
