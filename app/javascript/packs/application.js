93dbff394bea1910b2af987d59a3792d1104fec5
pisces2336 <pisces2336@gmail.com>
2023-02-25 12:56:58 +0000

    chore: add font-awesome 6.3.0

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ import "channels"
 import "bootstrap";
 import "../stylesheets/application.scss";
 
+import '@fortawesome/fontawesome-free/js/all';
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
