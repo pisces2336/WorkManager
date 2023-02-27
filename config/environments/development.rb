626bbbe6864d6b6578594126c363d399f07a1164
pisces2336 <pisces2336@gmail.com>
2023-02-27 05:35:04 +0000

    chore: set the encrypted smtp_settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -74,5 +74,18 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "fe5997b83f174283a1c168f11c344fe4.vfs.cloud9.us-east-1.amazonaws.com"
+  host = "fe5997b83f174283a1c168f11c344fe4.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << host
+  Rails.application.routes.default_url_options[:host] = host
+
+  config.mailer_sender = 'workmanager36@gmail.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => Rails.application.credentials[:smtp_settings][:user_name],
+    :password => Rails.application.credentials[:smtp_settings][:password],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
